4df384b2b07a727e3db3590f32ed3953e1d20b7d
samuel moreira <samuelmoreiradelima.20@gmail.com>
2026-08-04 21:21:07 -0700

    Adiciona dados do candidato e vagas

---
+++ projeto-01/simulador-compatibilidade.js
@@ -0,0 +1,14 @@
+const candidate = {
+    name: "samuel",
+    skills: ["javaScript"]
+}
+   console.log(candidate);
+
+   const vacancy = {
+    empresa: "stark industries",
+    cargo: "front-end junior",
+    requisitos: [ "javaScript", "HTML", "CSS" ]
+}
+   console.log(vacancy);
+
+   
