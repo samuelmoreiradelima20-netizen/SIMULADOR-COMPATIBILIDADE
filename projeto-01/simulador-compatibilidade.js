a0e03047f01d237740a4c61bf49467f988359091
samuel moreira <samuelmoreiradelima.20@gmail.com>
2026-08-07 23:50:42 -0700

    Adiciona metodos de analise de vagas

--- projeto-01/simulador-compatibilidade.js
+++ projeto-01/simulador-compatibilidade.js
@@ -57,5 +57,56 @@ function analyzeCompatibility(candidate, vacancy) {
     console.log("Habilidades faltantes:", missingSkills);
     console.log("Habilidades encontradas:", matchedSkills);
     console.log("Porcentagem de compatibilidade:", compatibilityPercentage.toFixed(2) + "%");
+     return {
+        matchedSkills: matchedSkills,
+        missingSkills: missingSkills,
+        compatibilityPercentage: compatibilityPercentage
+    };
+    
 }
 analyzeCompatibility(candidate1, vacancy);
+
+const vacancies = [
+    {
+        empresa: "stark industries",
+        cargo: "front-end junior",
+        requisitos: ["JavaScript", "HTML", "CSS"]
+        },
+
+         {
+        empresa: "Wayne Enterprises",
+        cargo: "front-end junior",
+        requisitos: ["JavaScript", "HTML"]
+    },
+];
+
+console.log("vacancies", vacancies);
+
+//for (let i = 0; i < vacancies.length; i++) {
+//    console.log("Empresa:", vacancies[i].empresa);
+//    analyzeCompatibility(candidate1, vacancies[i]);
+//}
+
+const results = vacancies.map(vacancy => {
+    const result = analyzeCompatibility(candidate1, vacancy);
+    return{
+        empresa: vacancy.empresa,
+        ...result
+    };
+});
+console.log("resultados", results);
+
+const bestVacancy = results.reduce((best, current) => {
+    return current.compatibilityPercentage > best.compatibilityPercentage ? current : best;
+}, { compatibilityPercentage: 0 });
+
+console.log("A melhor vaga é:", bestVacancy); 
+console.log("com", bestVacancy.compatibilityPercentage.toFixed(2) + "% de compatibilidade.");
+
+const wayneVacancy = results.find(vacancy => vacancy.empresa === "Wayne Enterprises");
+console.log("Vaga na Wayne Enterprises:", wayneVacancy);
+
+const hasAllSkills = vacancies[1].requisitos.every(
+    requisito => candidate1.skills.includes(requisito)
+);
+console.log("O candidato possui todas as habilidades para a vaga da Wayne Enterprises?", hasAllSkills);
